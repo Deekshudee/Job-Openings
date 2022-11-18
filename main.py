8089587098389bb805cce525e0c83e23727351ff
Deekshitha M <117582391+Deekshudee@users.noreply.github.com>
2022-11-18 15:06:21 +0000

    added api data

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+JOBS = [
+{
+  'id' : 1,
+  'postion': 'data analyst',
+  'location':'banglore India',
+  'salary': 'Rs. 10,00,000'
+},
+{
+  'id': 2,
+  'postion':'data engineer',
+  'location':'delhi India',
+  'salary': 'Rs. 12,00,000'
+},
+{
+  'id':3,
+  'postion':'backend engineer',
+  'location':'pune India',
+  'salary':'Rs. 15,00,000'
+}
+]
+
+@app.route("/api/jobs")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+                        
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', debug=True)
